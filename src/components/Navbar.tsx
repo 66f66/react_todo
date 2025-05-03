904d455d42ac51156049cfe670fc68e97be9633e
66f66 <3303700@gmail.com>
2025-05-04 03:41:34 +0900

    Remove unused lib, Add github link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,21 +10,28 @@ import { Button } from './ui/button'
 export const Navbar = () => {
   return (
     <header className='fixed top-0 z-50 w-full bg-white/80 shadow-sm backdrop-blur-sm'>
-      <nav className='mx-auto grid min-h-[70px] max-w-6xl grid-cols-4 px-4 py-4'>
-        <div>
+      <nav className='mx-auto grid min-h-[70px] max-w-6xl grid-cols-8 px-4 py-4'>
+        <div className='col-span-2 flex items-center gap-1'>
           <Link
             to='/'
             className='text-xl font-bold'
           >
             Home
           </Link>
+          <a
+            className='hidden text-blue-500 md:inline'
+            href='https://github.com/66f66/react_todo'
+            target='_blank'
+          >
+            코드
+          </a>
         </div>
 
-        <div className='col-span-2'>
+        <div className='col-span-4'>
           <TodoSearch />
         </div>
 
-        <div className='flex items-center justify-end gap-2'>
+        <div className='col-span-2 flex items-center justify-end gap-2'>
           <TodosStatus />
           <UserButton />
         </div>
@@ -46,7 +53,7 @@ const TodosStatus: FC = () => {
   }
 
   return (
-    <div>
+    <div className='hidden md:inline'>
       <Loader />
     </div>
   )
